7773236572f432cffa6095292cb37f88801b762b
LOVISH GOYAL <112848018+LOVISH066@users.noreply.github.com>
2024-10-01 00:32:07 +0530

    Time: 1 ms (16.81%), Space: 42.3 MB (36.61%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.java
@@ -1,10 +1,12 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int count = 0;
-        for(int i = 0; i < hours.length; i++)
+        for(int hour : hours)
         {
-            if(hours[i] >= target)
+            if(hour >= target)
+            {
                 count ++;
+            }
         }
         return count;
         }
